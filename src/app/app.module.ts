5f2d911c8ef36e8a3dd6d2d19e295b5fa6f2f83e
chandini1995 <chandini.metagogy@gmail.com>
2021-06-24 18:42:51 +0530

    changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IfComponent } from './if/if.component';
+import { ProductComponent } from './product/product.component';
+import { SellerComponent } from './seller/seller.component';
+import { SwitchComponent } from './switch/switch.component';
+import { ForComponent } from './for/for.component';
+import { CountriesComponent } from './countries/countries.component';
+import { CountriesService} from './countries.service'
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    SellerComponent,
+    IfComponent,
+    SwitchComponent,
+    ForComponent,
+    CountriesComponent,
+  
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CountriesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
